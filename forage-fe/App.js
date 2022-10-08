d3fc9c6c2f0815267bd41137c6355080c0619352
Geoff24 <geoffaj24@gmail.com>
2022-10-08 00:34:13 -0500

    better splash

--- forage-fe/App.js
+++ forage-fe/App.js
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AnimatedSplash from "react-native-animated-splash-screen";
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+
+  setTimeout(() => {
+    setLoading(true);
+  }, 2000);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AnimatedSplash
+      translucent={false}
+      isLoaded={loading}
+      logoImage={require("./assets/splash.png")}
+      backgroundColor={"#EB3737"}
+      logoHeight={500}
+      logoWidth={500}
+    >
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </AnimatedSplash>
   );
 }
 
