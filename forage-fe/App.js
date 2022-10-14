119029938919ff8b8d96d749e63da05b27b76f55
Tomilola Olaoye <tomi@Tomilolas-MacBook-Pro.local>
2022-10-13 23:05:21 -0500

    moved splash screen code to separate file

--- forage-fe/App.js
+++ forage-fe/App.js
@@ -1,29 +1,14 @@
-import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import AnimatedSplash from "react-native-animated-splash-screen";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import SplashScreen from './Screen/SplashScreen';
 
 export default function App() {
-  const [loading, setLoading] = useState(false);
-
-  setTimeout(() => {
-    setLoading(true);
-  }, 2000);
-
   return (
-    <AnimatedSplash
-      translucent={false}
-      isLoaded={loading}
-      logoImage={require("./assets/splash.png")}
-      backgroundColor={"#EB3737"}
-      logoHeight={500}
-      logoWidth={500}
-    >
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </AnimatedSplash>
+    <View style={styles.container}>
+      
+      <SplashScreen/>
+    </View>
   );
 }
 
---
+++ forage-fe/Screen/SplashScreen.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AnimatedSplash from "react-native-animated-splash-screen";
+
+const SplashScreen = () => {
+  const [loading, setLoading] = useState(false);
+
+  setTimeout(() => {
+    setLoading(true);
+  }, 2000);
+
+  return (
+    <AnimatedSplash
+      translucent={false}
+      isLoaded={loading}
+      logoImage={require("./../assets/splash.png")}
+      backgroundColor={"#EB3737"}
+      logoHeight={500}
+      logoWidth={500}
+    >
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </AnimatedSplash>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default SplashScreen;
