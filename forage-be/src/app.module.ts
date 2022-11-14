f043eb0241564b76a0e07fd2f92c3b574e4abd04
Brandon Domangue <12216617+lowtied@users.noreply.github.com>
2022-11-14 15:35:13 -0600

    add prisma user model, create user registration route

--- forage-be/src/app.module.ts
+++ forage-be/src/app.module.ts
@@ -1,25 +1,16 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './forage-users/users.module';
-import { JwtService,JwtModule } from '@nestjs/jwt';
-import { secret } from './constants'
-import { UsersService } from './forage-users/users.service';
-import { User, UserSchema } from './forage-users/users.model';
-import { UsersController } from './forage-users/users.controller';
+import { PrismaService } from './prisma.service';
+import { UserService } from './user.service';
 
 @Module({
-  imports: [UserModule,MongooseModule.forRoot(
-    'mongodb+srv://loremipsum:forage123@cluster0.5rodjye.mongodb.net/forage?retryWrites=true&w=majority',
-    
-  ),
-  JwtModule.register({
-    secret,
-    signOptions: {expiresIn: 60}
-  }),
-],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService,JwtService],
+  providers: [
+      PrismaService,
+      UserService
+  ],
 })
 export class AppModule {}
+
+//vim: ft=typescript ts=2 sw=2 sts=-1 sta et
--- forage-be/src/main.ts
+++ forage-be/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Forage API')
+    .setDescription('Forage API Swagger browser.')
+    .setVersion('1.0')
+    .addTag('forage')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
 
+// vim: ft=typescript ts=2 sw=2 sts=-1 sta et
