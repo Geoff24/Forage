51d379c4633bcf548cc899a9ed77f59b33afc380
Brandon Domangue <12216617+lowtied@users.noreply.github.com>
2022-11-30 10:58:57 -0600

    update main app module

--- forage-be/src/app.module.ts
+++ forage-be/src/app.module.ts
@@ -10,6 +10,8 @@ import { UsersModule } from './users/users.module';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { JwtModule } from '@nestjs/jwt';
+import { IngredientsModule } from './ingredients/ingredients.module';
+import { LikesModule } from './likes/likes.module';
 
 @Module({
   imports: [
@@ -20,6 +22,8 @@ import { JwtModule } from '@nestjs/jwt';
     UsersModule,
     PrismaModule,
     JwtModule,
+    IngredientsModule,
+    LikesModule,
   ],
   controllers: [AppController, AuthController, UsersController],
   providers: [PrismaService, AuthService, UsersService],
